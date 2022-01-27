2745035eda40c027a60636f1f544cc5d48d016f0
Sudheer Pamula <sudheer.pamula@gmail.com>
2022-01-27 00:17:51 +0000

    Test of API - Successful

---
+++ app.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+from model import SentimentModel, SentimentQueryModel
+
+app = FastAPI()
+model = SentimentModel()
+
+@app.post('/predict')
+def predict(data: SentimentQueryModel):
+    data = data.dict()
+    polarity, subjectivity = model.get_sentiment(data['text'])
+    return {
+        'polarity': polarity,
+        'subjectivity': subjectivity
+    }
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Test of FastAPI with Spacy NLP Sentiment Indexing
+
+Example derived from - <a href="https://towardsdatascience.com/deploying-an-ml-model-with-fastapi-a-succinct-guide-69eceda27b21">here</a>
+
+### Changelog
+27-01-2022 - Refactored code from example to work with updated version of Spacy
